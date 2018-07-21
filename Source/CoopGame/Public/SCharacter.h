c2b6fdfd45fb30dd529dde7245462892b62e8e39
C.y <yangruihan@vip.qq.com>
2018-07-21 15:41:48 +0800

    1. 角色添加Camera组件 2. 地图重命名

--- Source/CoopGame/Public/SCharacter.h
+++ Source/CoopGame/Public/SCharacter.h
@@ -6,6 +6,9 @@
 #include "GameFramework/Character.h"
 #include "SCharacter.generated.h"
 
+class UCameraComponent;
+class USpringArmComponent;
+
 UCLASS()
 class COOPGAME_API ASCharacter : public ACharacter
 {
@@ -22,6 +25,14 @@ protected:
     void MoveForward(float Value);
 
     void MoveRight(float Value);
+
+protected:
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadonly, Category = "Components")
+    UCameraComponent* CameraComp;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadonly, Category = "Components")
+    USpringArmComponent* SpringArmComp;
 
 public:	
 	// Called every frame
