9d5a4c546d748e84017d535797f861d99fe1406a
C.y <yangruihan@vip.qq.com>
2018-07-21 16:25:56 +0800

    1. 增加跳跃功能及动画

--- Source/CoopGame/Private/SCharacter.cpp
+++ Source/CoopGame/Private/SCharacter.cpp
@@ -69,5 +69,7 @@ void ASCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponen
 
     PlayerInputComponent->BindAction("Crouch", IE_Pressed, this, &ASCharacter::BeginCrouch);
     PlayerInputComponent->BindAction("Crouch", IE_Released, this, &ASCharacter::EndCrouch);
+ 
+    PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ASCharacter::Jump);
 }
 
