d7178d7d3aaf750a7db94c34896ae404772d3589
C.y <yangruihan@vip.qq.com>
2018-07-23 07:45:15 +0800

    1. 添加 Health 组件

---
+++ Source/CoopGame/Private/SHealthComponent.cpp
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Public/SHealthComponent.h"
+
+
+// Sets default values for this component's properties
+USHealthComponent::USHealthComponent()
+{
+}
+
+
+// Called when the game starts
+void USHealthComponent::BeginPlay()
+{
+	Super::BeginPlay();
+}
---
+++ Source/CoopGame/Public/SHealthComponent.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "SHealthComponent.generated.h"
+
+
+UCLASS( ClassGroup=(COOP), meta=(BlueprintSpawnableComponent) )
+class COOPGAME_API USHealthComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	USHealthComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadonly, Category = "Health")
+    float Health;
+};
